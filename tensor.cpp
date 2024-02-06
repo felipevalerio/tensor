59c9472d3a4155283d1a0fda9dc8be639a0233ea
Felipe <felipevalerioderamos@gmail.com>
2024-02-06 14:59:20 -0300

    random generation of values for tensors

--- tensor.cpp
+++ tensor.cpp
@@ -4,12 +4,12 @@
 int main() {
 
 
-	Eigen::Tensor<int, 2> tensor(3, 2);
-	tensor.setValues({{1, 1}, {3, 3}, {9, 9}}); 
+	Eigen::Tensor<int, 2> tensor1(4, 2), tensor2(2, 4);
+	tensor1.setRandom();
+	tensor2.setRandom(); 
 
-	std::cout << "tensor:\n\n" << tensor << "\n\n";
-
-	std::cout << "tamanho do tensor: " << tensor.size() << "\n\n";
+	std::cout << "tensor 1:\n\n" << tensor1 << "\n\n";
+	std::cout << "tensor 2: \n\n" << tensor2 << "\n\n";
 
 	return 0;
 }
