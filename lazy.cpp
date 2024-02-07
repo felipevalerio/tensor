4032e6650ff8364183074d1e36d5a2cfab18a54a
Felipe <felipevalerioderamos@gmail.com>
2024-02-07 17:29:42 -0300

    typos

--- lazy.cpp
+++ lazy.cpp
@@ -5,11 +5,11 @@ int main() {
 
 	// An example of lazy evalution with tensors
 	// Lazy evaluation is the ideia that a operation will not be directly assigned (evaluated)
-	// to a variable (for example) until the same variable be used (assigned)
+	// to a variable (for example) until the same variable be used
 
 	// in the example auto tensorC was not assigned with the sum of the operation
-	// instead it created a computation object that knows how to calculate the given some operation
-	// and it will do that, once tensorC is used (assigned) which happens in the line 19
+	// instead it created a computation object that knows how to calculate the given sum operation
+	// and it will do that, once tensorC is used in the code
 
 	Eigen::Tensor<int, 2> tensorA(4, 2);
 	Eigen::Tensor<int, 2> tensorB(4, 2);
